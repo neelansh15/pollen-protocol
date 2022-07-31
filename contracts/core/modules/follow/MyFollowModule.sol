3dc3de8f9191cb790a67054b473c8d446d411000
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-08-01 03:02:18 +0530

    ft: test follow module

---
+++ contracts/core/modules/follow/MyFollowModule.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity 0.8.10;
+
+import {ModuleBase} from '../ModuleBase.sol';
+import {IFollowModule} from '../../../interfaces/IFollowModule.sol';
+import {FollowValidatorFollowModuleBase} from './FollowValidatorFollowModuleBase.sol';
+
+/**
+ * @title MyFollowModule
+ * @author Neelansh Mathur
+ *
+ **/
+contract MyFollowModule is IFollowModule, FollowValidatorFollowModuleBase {
+    error PasscodeInvalid();
+
+    mapping(uint256 => uint256) internal _passcodeByProfile;
+
+    constructor(address hub) ModuleBase(hub) {}
+
+    function initializeFollowModule(uint256 profileId, bytes calldata data)
+        external
+        override
+        onlyHub
+        returns (bytes memory)
+    {
+        uint256 passcode = abi.decode(data, (uint256));
+        _passcodeByProfile[profileId] = passcode;
+        return data;
+    }
+
+    function processFollow(
+        address, // follower
+        uint256 profileId,
+        bytes calldata data
+    ) external view override {
+        uint256 passcode = abi.decode(data, (uint256));
+        if (passcode != _passcodeByProfile[profileId]) revert PasscodeInvalid();
+    }
+
+    function followModuleTransferHook(
+        uint256 profileId,
+        address from,
+        address to,
+        uint256 followNFTTokenId
+    ) external override {}
+}
